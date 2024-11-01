992d0f5e42fa72022feda6611d01ed433ebf8811
harioctav <aldiamahari16@gmail.com>
2024-11-02 04:12:10 +0700

    Episode 6: Create Routes, Link and Controllers

---
+++ resources/js/Pages/Admin/Users/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="List Users" />
+
+    <AdminLayout>
+        <div class="py-4">
+            <h1>Users Index Page</h1>
+        </div>
+    </AdminLayout>
+</template>
